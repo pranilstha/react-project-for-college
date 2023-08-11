c1cb93e97e256446f316eb51a0ee4fc9452475d8
shini <shini@pop-os.localdomain>
2023-08-11 21:30:43 +0545

    Moved HeroSection codes into BookList

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -1,13 +1,13 @@
 import './App.css'
+import BookList from './components/BookList/BookList'
 import Footer from './components/Footer/Footer'
-import HeroSection from './components/HeroSection/HeroSection'
 import NavigationBar from './components/NavigationBar/NavigationBar'
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <HeroSection />
+      <BookList />
       <Footer />
     </>
   )
